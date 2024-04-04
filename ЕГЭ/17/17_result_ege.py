af7398c47cf2891260463bf4e194a2bfe3e42da9
Misha Voroshilolov <unluckym0ve@gmail.com>
2024-04-04 20:56:47 +0300

    update 04.04.2024

--- ЕГЭ/17/17_result_ege.py
+++ ЕГЭ/17/17_result_ege.py
@@ -1,11 +1,34 @@
 #задание № 17 
+
 with open('17_10100.txt','r',encoding='utf-8') as f:
+
+    def c(x):
+        return 99<abs(x)<=999
+            
+
     result = list()
-    counter=0
     before = [int(i) for i in f]
     max_coord = max([i for i in before if abs(i)%100 == 13])
+
     for i in range(len(before)-2):
-        if ((99 < abs(before[i]) <= 999)+ (99 < abs(before[i+1])<=999) + (99<abs(before[i+2])<=999))==2 and sum([before[i],before[i+1],before[i+2]])<=max_coord:
-           counter+=1
+        if (c(before[i+1])+ c(before[i])+c(before[i+2]))==2 and sum([before[i],before[i+1],before[i+2]])<=max_coord:
            result.append(sum([before[i],before[i+1],before[i+2]]))
-    print(counter,max(result))
+
+    print(len(result),max(result))
+
+
+# 4 вариант от Яндекса 
+with open('17.txt','r') as  f:
+
+    result = []
+    a = [int(i) for i in f]
+    max_coord = max([i for i in a if len(str(i)) == 4])
+
+    def f(x):
+        return abs(x) % 10 == 3 or abs(x) % 10 == 5
+
+    for i in range(len(a)-2):
+        if (f(a[i]) + f(a[i+2]) + f(a[i+1]))>1 and (a[i]*a[i+1]*a[i+2])<=max_coord**3:
+            result.append(sum((a[i],a[i+1],a[i+2])))
+
+    print(max(result),len(result))
