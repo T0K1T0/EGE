b039010c9d09a9f40ee9923c7ae30b0512719fd4
Misha Voroshilolov <unluckym0ve@gmail.com>
2024-05-10 20:19:20 +0300

    update 10.05.2024

--- ЕГЭ/24/24_result_ege.py
+++ ЕГЭ/24/24_result_ege.py
@@ -21,7 +21,7 @@ with open('24-14100_14100.txt','r') as s:
                     arr[i] = max(dp[i], arr[i-len(j)] + j,key=len)
 
 print(len(max(arr, key = len)))
-'''
+
 with open('24 (1).txt') as f:
     p = ''.join([i for i in f])
     max_len = 0
@@ -36,3 +36,21 @@ with open('24 (1).txt') as f:
     if max_len < cur_len:
         max_len = cur_len
     print(max_len)
+'''
+#вариант № 6 от яндекса
+with open('24 (2).txt','r') as f:
+
+    s = ''.join([i for i in f]).replace('E','A').replace('O','A').replace('U','A').replace('L','B').replace('N','B').replace('M','B').replace('K','B')
+
+    max_lenth,now_lenth = 0,0
+    for i in range(len(s)-1):
+        if 'AA' in s[i]+s[i+1] or 'BB' in s[i]+s[i+1]:
+            max_lenth = max(max_lenth,now_lenth+1)
+            now_lenth = 0
+        else:
+            now_lenth+=1
+
+
+        max_lenth = max(max_lenth,now_lenth)
+
+    print(max_lenth)
